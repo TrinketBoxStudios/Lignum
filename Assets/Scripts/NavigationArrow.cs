d81ef4b86b2718cd5bb56f0c0cdded71aac805ac
ryderhanson <ryder.hanson@gmail.com>
2014-01-15 17:28:46 -0800

    Implemented simple navigation button and 5 initial scenes for simple gameflow

---
+++ Assets/Scripts/NavigationArrow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigationArrow : MonoBehaviour {
+
+	public float fadeTime = 1.0f;
+	public string levelToLoad = "none";
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnMouseDown()
+	{
+		Application.LoadLevel(levelToLoad);
+	}
+}
