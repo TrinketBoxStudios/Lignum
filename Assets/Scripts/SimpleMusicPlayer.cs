eee0567012e944bf82323350452380d644dbedc5
ryderhanson <ryder.hanson@gmail.com>
2014-02-04 16:06:57 -0800

    Level Progress, Text Roughing, and Soundtrack In
    Added and laid out Sprigs interior, added text boxes, and added a simple
    soundtrack to play.

---
+++ Assets/Scripts/SimpleMusicPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMusicPlayer : MonoBehaviour {
+
+	public AudioClip[] clips;
+
+	private AudioSource _audSrc;
+	// Use this for initialization
+	void Start () 
+	{
+		_audSrc = GetComponent<AudioSource>();
+		PlaySound("SoundtrackRough");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void PlaySound( string name )
+	{
+		foreach(AudioClip clip in clips)
+		{
+			if(clip.name == name)
+			{
+				_audSrc.clip = clip;
+				_audSrc.Play();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/SingleToggleAnimaion.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleToggleAnimaion : MonoBehaviour 
+{
+	public string[] childInteractionDialogue;
+	public string[] adultInteractionDialogue;
+	public string[] elderlyInteractionDialogue;
+
+	//Starting sprite frame (Assumed)
+	public Sprite frameOne;
+	//Frame to toggle to
+	public Sprite frameTwo;
+
+	private int _currentFrame = 0;
+
+	private DialogueManager _dlgBox;
+
+	private SpriteRenderer _sprRender;
+	// Use this for initialization
+	void Start () 
+	{
+		_sprRender = GetComponent<SpriteRenderer>();
+
+		_dlgBox = FindObjectOfType<DialogueManager>();
+	}
+
+	void OnMouseDown()
+	{
+		ToggleAnimation();
+	}
+
+	void ToggleAnimation()
+	{
+		if(_currentFrame == 0)
+		{
+			_sprRender.sprite = frameTwo;
+			_currentFrame = 1;
+
+			//Only send this is we're 'opening' the object, 
+			//as most objects go from closed to open on the first frame
+			SendTextToDialogueBox();
+		}
+		else if(_currentFrame == 1)
+		{
+			_sprRender.sprite = frameOne;
+			_currentFrame = 0;
+
+			_dlgBox.HideMe();
+		}
+	}
+
+	void SendTextToDialogueBox()
+	{
+		switch(GameStateManager.GetInstance().GetAge())
+		{
+		case GameStateManager.Age.CHILD:
+		{
+			_dlgBox.InitializeAndShowText(childInteractionDialogue);
+			break;
+		}
+			
+		case GameStateManager.Age.ADULT:
+		{
+			_dlgBox.InitializeAndShowText(adultInteractionDialogue);
+			break;
+		}
+			
+		case GameStateManager.Age.ELDERLY:
+		{
+			_dlgBox.InitializeAndShowText(elderlyInteractionDialogue);
+			break;
+		}
+		}
+	}
+}
